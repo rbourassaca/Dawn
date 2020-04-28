9e9f4aee311e1d3108807fe867ef23db650a7b64
Raphael Bourassa <bourassa.raphael@outlook.com>
2020-04-27 23:08:48 -0400

    Changing the admin footer text.

--- dawn.php
+++ dawn.php
@@ -12,8 +12,13 @@ GitHub Plugin URI: https://github.com/rbourassaca/Dawn
 
 add_action('admin_enqueue_scripts', 'admin_theme');
 add_action('login_enqueue_scripts', 'admin_theme');
+add_filter('admin_footer_text', 'footerText');
 
 function admin_theme () {
-    wp_enqueue_style('my-admin-theme', plugins_url('style/style.css', __FILE__));
+    wp_enqueue_style('rbourassa_admin_theme', plugins_url('style/style.css', __FILE__));
+}
+
+function footerText() {
+    echo 'Site web créé par <a href="https://rbourassa.ca" target="_blank">Raphael Bourassa</a>';
 }
 ?>
