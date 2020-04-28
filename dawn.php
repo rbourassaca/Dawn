fb86b98f9ad3df05766eb6b49bed29bc08d12c0d
Raphael Bourassa <bourassa.raphael@outlook.com>
2020-04-28 14:09:19 -0400

    Adding the theme to the front-end admin bar.

--- dawn.php
+++ dawn.php
@@ -11,6 +11,7 @@ GitHub Plugin URI: https://github.com/rbourassaca/Dawn
 */
 
 add_action('admin_enqueue_scripts', 'admin_theme');
+add_action( 'wp_enqueue_scripts', 'admin_theme');
 add_action('login_enqueue_scripts', 'admin_theme');
 add_filter('admin_footer_text', 'footerText');
 
